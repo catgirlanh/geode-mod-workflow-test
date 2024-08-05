9128ed6ed5b7f38ea08959d3ef694b17fe985e1e
Nya Meow <nyameowmeow@icloud.com>
2024-08-05 22:07:29 +0700

    v1.6.0
    go go build machine!!!

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,35 @@
-/**
- * Include the Geode headers.
- */
-#include <Geode/Geode.hpp>
+#include <Geode/modify/MenuLayer.hpp>
+#include <Geode/utils/web.hpp>
+#include <Geode/loader/Event.hpp>
 
-/**
- * Brings cocos2d and all Geode namespaces to the current scope.
- */
 using namespace geode::prelude;
 
-#include <Geode/modify/PlayLayer.hpp>
+class $modify(MenuLayer) {
+    struct Fields {
+        EventListener<web::WebTask> m_listener;
+    };
 
-class $modify(PlayLayer) {
-	virtual void destroyPlayer(PlayerObject* p0, GameObject* p1) {}
+// everything from here up is boilerplate
+    bool init() {
+        if (!MenuLayer::init()) {
+            return false;
+        }
+
+        m_fields->m_listener.bind([] (web::WebTask::Event* e) {
+            if (web::WebResponse* res = e->getValue()) {
+// if the request was successful 
+                log::info("{}", res->string().unwrapOr("Uh oh!"));
+            } else if (web::WebProgress* p = e->getProgress()) {
+// if the request is in progress, say how far along it is
+                log::info("progress: {}", p->downloadProgress().value_or(0.f));
+            } else if (e->isCancelled()) {
+// say if the request errored or was canceled
+                log::info("The request was cancelled... So sad :(");
+            }
+        });
+
+// logic for starting the request
+        auto req = web::WebRequest(); m_fields->m_listener.setFilter(req.get("https://pastebin.com/raw/vNi1WHNF"));
+        return true;
+    }
 };
